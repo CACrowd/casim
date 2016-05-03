a1f4d739eb892143c4c4c31b86f661232ed7d84b
GrgrLmml <gregor.laemmel@gmail.com>
2016-05-03 15:27:46 +0200

    NeighbourhoodUtilityTest currently not functioning

---
+++ src/test/java/pedca/utility/NeighbourhoodUtilityTest.java
@@ -0,0 +1,52 @@
+package pedca.utility;
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2014 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+import org.junit.Test;
+import pedCA.environment.grid.GridPoint;
+import pedCA.environment.grid.neighbourhood.Neighbourhood;
+import pedCA.utility.NeighbourhoodUtility;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by laemmel on 03/05/16.
+ */
+public class NeighbourhoodUtilityTest {
+
+
+	@Test
+	public void testCalculateMoorNeighbourhood() {
+		GridPoint neighbour = new GridPoint(5,9);
+		Neighbourhood nb = NeighbourhoodUtility.calculateMooreNeighbourhood(neighbour);
+
+		assertThat(nb.size(),is(equalTo(8)));
+	}
+
+	@Test
+	public void testCalculateVonNeumannNeighbourhood() {
+		GridPoint neighbour = new GridPoint(8,9);
+		Neighbourhood nb = NeighbourhoodUtility.calculateVonNeumannNeighbourhood(neighbour);
+
+		assertThat(nb.size(),is(equalTo(4)));
+	}
+}
