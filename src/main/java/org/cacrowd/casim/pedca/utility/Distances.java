dbcfd27a8f372eb32caf36b817f2ac01d8e5ff81
GrgrLmml <gregor.laemmel@gmail.com>
2016-08-09 13:26:30 +0200

    bugfix distances btwn 2 grid points

--- src/main/java/org/cacrowd/casim/pedca/utility/Distances.java
+++ src/main/java/org/cacrowd/casim/pedca/utility/Distances.java
@@ -18,8 +18,8 @@ import org.cacrowd.casim.pedca.environment.network.Coordinate;
 public class Distances {
 
 	public static Double EuclideanDistance(GridPoint gp1, GridPoint gp2) {
-		return Math.sqrt((gp1.getX()-gp2.getX())^2+(gp1.getY()-gp2.getY())^2);
-	}
+        return Math.sqrt((gp1.getX() - gp2.getX()) * (gp1.getX() - gp2.getX()) + (gp1.getY() - gp2.getY()) * (gp1.getY() - gp2.getY()));
+    }
 
 	public static double EuclideanDistance(Coordinate c1, Coordinate c2) {
 		return Math.sqrt(Math.pow(c1.getX()-c2.getX(),2)+Math.pow(c1.getY()-c2.getY(),2));
