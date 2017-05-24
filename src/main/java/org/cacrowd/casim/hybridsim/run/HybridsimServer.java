4c6a7d10f1c72caf35dbe9804c3a831b2360ae42
GrgrLmml <gregor.laemmel@gmail.com>
2017-05-24 14:02:48 +0200

    hybridsim

---
+++ src/main/java/org/cacrowd/casim/hybridsim/run/HybridsimServer.java
@@ -0,0 +1,22 @@
+/*
+ * casim, cellular automaton simulation for multi-destination pedestrian
+ * crowds; see www.cacrowd.org
+ * Copyright (C) 2016-2017 CACrowd and contributors
+ *
+ * This file is part of casim.
+ * casim is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ *
+ */
+
+package org.cacrowd.casim.hybridsim.run;
+
+public class HybridsimServer {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/cacrowd/casim/hybridsim/run/HybridsimTestClient.java
@@ -0,0 +1,73 @@
+/*
+ * casim, cellular automaton simulation for multi-destination pedestrian
+ * crowds; see www.cacrowd.org
+ * Copyright (C) 2016-2017 CACrowd and contributors
+ *
+ * This file is part of casim.
+ * casim is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ *
+ */
+
+package org.cacrowd.casim.hybridsim.run;
+
+import org.cacrowd.casim.hybridsim.grpc.GRPCExternalClient;
+import org.cacrowd.casim.proto.HybridSimProto;
+
+public class HybridsimTestClient {
+
+    public static void main(String args[]) {
+
+        HybridSimProto.Scenario sc = createScenario();
+
+        GRPCExternalClient client = new GRPCExternalClient("localhost", 9000);
+        client.getBlockingStub().initScenario(sc);
+
+
+    }
+
+    private static HybridSimProto.Scenario createScenario() {
+
+        HybridSimProto.Scenario.Builder sb = HybridSimProto.Scenario.newBuilder();
+        HybridSimProto.Edge.Builder eb = HybridSimProto.Edge.newBuilder();
+        HybridSimProto.Coordinate.Builder cb = HybridSimProto.Coordinate.newBuilder();
+
+        cb.setX(0);
+        cb.setY(0);
+        eb.setC0(cb.build());
+        cb.setX(20);
+        eb.setC1(cb.build());
+        eb.setType(HybridSimProto.Edge.Type.OBSTACLE);
+        sb.addEdges(eb.build());
+
+        cb.setX(0);
+        cb.setY(4);
+        eb.setC0(cb.build());
+        cb.setX(20);
+        eb.setC1(cb.build());
+        eb.setType(HybridSimProto.Edge.Type.OBSTACLE);
+        sb.addEdges(eb.build());
+
+        cb.setX(0);
+        cb.setY(4);
+        eb.setC0(cb.build());
+        cb.setY(0);
+        eb.setC1(cb.build());
+        eb.setType(HybridSimProto.Edge.Type.OBSTACLE);
+        sb.addEdges(eb.build());
+
+        cb.setX(20);
+        cb.setY(0);
+        eb.setC0(cb.build());
+        cb.setY(4);
+        eb.setC1(cb.build());
+        eb.setType(HybridSimProto.Edge.Type.OBSTACLE);
+        sb.addEdges(eb.build());
+
+
+        return sb.build();
+    }
+}
